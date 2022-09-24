4665f9856a152fed0d1cdbbe77ee5c5612c69d42
CamiloTamayo <juanc.tamayoa@uqvirtual.edu.co>
2022-09-23 21:03:13 -0500

    First Commit

---
+++ ProyectoCuestionarioPT1/src/main/java/uniquindio/analisis/Proyectocuestionariopt1Application.java
@@ -0,0 +1,13 @@
+package uniquindio.analisis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyectocuestionariopt1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyectocuestionariopt1Application.class, args);
+	}
+
+}
